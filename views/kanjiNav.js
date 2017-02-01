558489cbdc0cf12480296e1ad2a7f3fa24168e65
maxint137 <maxint@hotmail.com>
2017-02-01 11:02:37 +0900

    Using a relative address for the API

--- views/kanjiNav.js
+++ views/kanjiNav.js
@@ -141,7 +141,7 @@ var kanjiNav;
         var d = $.Deferred();
         // http://localhost:3000/api/v1/word/食品
         // http://localhost:3000/api/v1/kanji/品
-        var query = "http://localhost:3000/api/v1/" + type.type + "/" + id + (jlptFilter ? '?JLPT=' + jlptFilter : '');
+        var query = "/api/v1/" + type.type + "/" + id + (jlptFilter ? '?JLPT=' + jlptFilter : '');
         return $.get(query);
     }
 })(kanjiNav || (kanjiNav = {}));
--- views/kanjiNav.ts
+++ views/kanjiNav.ts
@@ -155,7 +155,7 @@ module kanjiNav {
 
         // http://localhost:3000/api/v1/word/食品
         // http://localhost:3000/api/v1/kanji/品
-        var query = "http://localhost:3000/api/v1/" + type.type + "/" + id + (jlptFilter ? '?JLPT=' + jlptFilter : '');
+        var query = "/api/v1/" + type.type + "/" + id + (jlptFilter ? '?JLPT=' + jlptFilter : '');
 
         return $.get(query);
     }
